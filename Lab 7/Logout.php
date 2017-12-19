9103cf67de686f6c35199c272b182c9d1f5c185b
Andrew Godfroy <andrew.godfroy@killerrin.com>
2017-12-18 20:32:29 -0500

    Completed Logout Requirements

--- Lab 7/Logout.php
+++ Lab 7/Logout.php
@@ -1,8 +1,14 @@
 <?php session_start(); ?>
 <?php include "Common/Header.php"; ?>
 
-<div class="container">
-    <h1>Logout</h1>
-</div>
+<?php
+// Completely annihilate the session
+session_unset();
+session_destroy();
+
+// Redirect to Index.php
+header("Location: Index.php");
+die();
+?>
 
 <?php include "Common/Footer.php"; ?>
