f32237e7feca71078333a744c254f74a61ab5768
Andrew Godfroy <andrew.godfroy@killerrin.com>
2017-12-18 20:33:29 -0500

    Add in some Temporary Logout Text

--- Lab 7/Logout.php
+++ Lab 7/Logout.php
@@ -1,6 +1,10 @@
 <?php session_start(); ?>
 <?php include "Common/Header.php"; ?>
 
+<div class="container">
+    <h1>Logout</h1>
+</div>
+
 <?php
 // Completely annihilate the session
 session_unset();
