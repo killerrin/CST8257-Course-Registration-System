568a00d3f3ab0389bb7fbe2fccf3286c62a2862f
Patrick Gingras <ging0044@algonquinlive.com>
2017-12-21 00:07:14 -0500

    Sorted course offers by semester

--- Lab 7/CourseSelection.php
+++ Lab 7/CourseSelection.php
@@ -15,6 +15,24 @@ $totalHours = (function($LoggedInUser) {
 
 $remainingHours = 16 - $totalHours;
 
+$semesters = array();
+
+$dbManager = new DBManager();
+$dbManager->connect();
+$courseOfferRepo = new DBCourseOfferRepository($dbManager);
+$courseRepo = new DBCourseRepository($dbManager);
+$semesterRepo = new DBSemesterRepository($dbManager);
+foreach($courseOfferRepo->getAll() as $courseOffer) {
+    $course = $courseRepo->getID($courseOffer[0])[0];
+    $semester = $semesterRepo->getID($courseOffer[1])[0];
+    $tmp = new CourseOffer($course, $semester);
+    if (!isset($semesters[$tmp->semester->semesterCode]))
+        $semesters[$tmp->semester->semesterCode] = array();
+    array_push($semesters[$tmp->semester->semesterCode], $tmp->course);
+}
+var_dump($semesters);
+$dbManager->close();
+
 ?>
 
 <div class="container">
@@ -23,7 +41,11 @@ $remainingHours = 16 - $totalHours;
     <p>You have registered <strong><?php echo $totalHours; ?></strong> hours for the selected semester.</p>
     <p>You can register <strong><?php echo $remainingHours; ?></strong> more hours of course(s) for the semester.</p>
     <p>Please note that the courses you have registered will not be displayed in the list.</p>
+    <select id="semesterSelect" class="form-control">
+        <?php foreach($semesters as $semester): ?>
 
+        <?php endforeach; ?>
+    </select>
 </div>
 
 <?php include "Common/Footer.php"; ?>
