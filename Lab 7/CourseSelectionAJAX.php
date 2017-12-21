1d453fa330412b6ffa7fd4a2cb067c1a50f274e7
Andrew Godfroy <andrew.godfroy@killerrin.com>
2017-12-21 00:51:53 -0500

    AJAX API completed

--- Lab 7/CourseSelectionAJAX.php
+++ Lab 7/CourseSelectionAJAX.php
@@ -1,7 +1,32 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: patrick
- * Date: 2017-12-21
- * Time: 00:30
- */
+include "Common/IncludeAll.php";
+header('Content-Type: application/json');
+
+$semesterCode = $_GET["semesterCode"];
+if (empty($semesterCode)) { die(); }
+
+$semesters = array();
+$terms = array();
+
+$dbManager = new DBManager();
+$dbManager->connect();
+$courseOfferRepo = new DBCourseOfferRepository($dbManager);
+$courseRepo = new DBCourseRepository($dbManager);
+$semesterRepo = new DBSemesterRepository($dbManager);
+
+$terms = $semesterRepo->getAll();
+
+foreach($courseOfferRepo->getAll() as $courseOffer) {
+    $course = $courseRepo->getID($courseOffer[0])[0];
+    $semester = $semesterRepo->getID($courseOffer[1])[0];
+    $tmp = new CourseOffer($course, $semester);
+    if (!isset($semesters[$tmp->semester->semesterCode]))
+        $semesters[$tmp->semester->semesterCode] = array();
+    array_push($semesters[$tmp->semester->semesterCode], $tmp->course);
+}
+//var_dump($semesters);
+$dbManager->close();
+
+echo json_encode($semesters[$semesterCode]);
+
+?>
