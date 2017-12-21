d7eafbe9f7db4f295afc1b03935ab767b30e099e
Patrick Gingras <ging0044@algonquinlive.com>
2017-12-21 16:33:51 -0500

    Added verification of POST data, for total hours and number of courses selected, along with error messages for each

--- Lab 7/CourseSelection.php
+++ Lab 7/CourseSelection.php
@@ -6,9 +6,23 @@ include "Common/Header.php";
 
 $LoggedInUser = isset($_SESSION["LoggedInUser"]) ? $_SESSION["LoggedInUser"] : (function() { header("Location: Login.php?returnUrl=".urlencode($_SERVER['REQUEST_URI'])); die();})();
 
+$selectCourseError = false;
+$totalHoursError = false;
+
+function calculateHours($LoggedInUser) {
+    $dbManager = new DBManager();
+    $dbManager->connect();
+    $hoursQueryResult = $dbManager->queryCustom("SELECT SUM(WeeklyHours) FROM Course JOIN (Registration, Student) ON ( Course.CourseCode = Registration.CourseCode AND Registration.StudentId = Student.StudentId ) WHERE Student.StudentId = '$LoggedInUser->studentID' GROUP BY Student.StudentId ;");
+    //var_dump($hoursQueryResult);
+    $hours = mysqli_fetch_row($hoursQueryResult);
+    return empty($hours) ? 0 : (int)$hours[0];
+};
 
 // Handle POST data
 
+if (isset($_POST['semester']) && count($_POST['course']) == 0)
+    $selectCourseError = true;
+var_dump($selectCourseError);
 if (!empty($_POST['course'])) {
     $dbManager = new DBManager();
     $dbManager->connect();
@@ -18,28 +32,32 @@ if (!empty($_POST['course'])) {
 
     $semester = $semesterRepo->getID($_POST['semester'])[0];
 
-    foreach($_POST['course'] as $course) {
-        //var_dump($courseRepo->getID($course)[0]);
-        //var_dump($semester);
-        $newRegistration = new Registration($LoggedInUser, $courseRepo->getID($course)[0], $semester);
+    $addHours = 0;
 
-        $registrationRepo->insert($newRegistration);
+    foreach($_POST['course'] as $course) {
+        $addHours += (int)$courseRepo->getID($course)[0]->weeklyHours;
+    }
+
+    if ($addHours + calculateHours($LoggedInUser) > 16)
+        $totalHoursError = true;
+
+    if (!$totalHoursError && !$selectCourseError) {
+        foreach ($_POST['course'] as $course) {
+            //var_dump($courseRepo->getID($course)[0]);
+            //var_dump($semester);
+            $newRegistration = new Registration($LoggedInUser, $courseRepo->getID($course)[0], $semester);
+
+            $registrationRepo->insert($newRegistration);
+        }
     }
     $dbManager->close();
 }
 
 // Calculate total course hours (and remaining) for current user
 
-$totalHours = (function($LoggedInUser) {
-    $dbManager = new DBManager();
-    $dbManager->connect();
-    $hoursQueryResult = $dbManager->queryCustom("SELECT WeeklyHours FROM Course JOIN (Registration, Student) ON ( Course.CourseCode = Registration.CourseCode AND Registration.StudentId = Student.StudentId ) WHERE Student.StudentId = '$LoggedInUser->studentID' GROUP BY Student.StudentId ;");
-    //var_dump($hoursQueryResult);
-    $hours = mysqli_fetch_row($hoursQueryResult);
-    return empty($hours) ? 0 : $hours[0];
-})($LoggedInUser);
+$totalHours = calculateHours($LoggedInUser);
 
-$remainingHours = 16 - (int)$totalHours;
+$remainingHours = 16 - $totalHours;
 
 
 // Get all terms for dropdown list
@@ -75,20 +93,28 @@ $dbManager->close();
         <br />
     </div>
     <div>
-            <table class="table">
-                <thead>
-                <row>
-                    <th>Code</th>
-                    <th>Course Title</th>
-                    <th>Hours</th>
-                    <th>Select</th>
-                </row>
-                </thead>
-                <tbody id="tbody">
-                <td colspan="4" align="center">Loading... Please wait</td>
-                </tbody>
-            </table>
-            <div class="form-group col-xs-4 col-xs-offset-8">
+        <table class="table">
+            <thead>
+            <row>
+                <th>Code</th>
+                <th>Course Title</th>
+                <th>Hours</th>
+                <th>Select</th>
+            </row>
+            </thead>
+            <tbody id="tbody">
+            <td colspan="4" align="center">Loading... Please wait</td>
+            </tbody>
+        </table>
+        <div class="col-xs-8">
+            <?php if($selectCourseError): ?>
+            <div class="alert alert-danger"><span class="glyphicon glyphicon-exclamation-sign"></span>&nbsp;Please select at least one course.</div>
+            <?php endif; ?>
+            <?php if($totalHoursError): ?>
+                <div class="alert alert-danger"><span class="glyphicon glyphicon-exclamation-sign"></span>&nbsp;You cannot exceed 16 hours per week.</div>
+            <?php endif; ?>
+        </div>
+            <div class="form-group col-xs-4">
                 <div class="col-xs-6">
                     <input type="submit" class="btn btn-primary btn-block" value="Submit" />
                 </div>
