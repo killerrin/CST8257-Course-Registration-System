81f1975c205f931ae284c66c2ad2981523160c72
Windjy E. Jean <Yuugami@users.noreply.github.com>
2017-12-20 23:42:17 -0500

    HTML - Hard Coded Table
    - Created Table in CurrentRegistration.php
    - Added dummy text/content into the table to style later.

--- Lab 7/CurrentRegistration.php
+++ Lab 7/CurrentRegistration.php
@@ -16,7 +16,98 @@ $dbManager->close();
 <div class="container">
     <h1>Current Registration</h1>
     <p>Hello <strong><?php echo $LoggedInUser->name; ?>!</strong> (not you? change user <a href="Login.php">here</a>), the followings are your current registrations</p>
-
+    <form action="/" method="post">
+        <table>
+            <tr>
+                <th>Year</th>
+                <th>Term</th>
+                <th>Course Code</th>
+                <th>Course Title</th>
+                <th>Hours</th>
+                <th>Select</th>
+            </tr>
+            <tr>
+                <td>2017</td>
+                <td>Fall</td>
+                <td>CAD8405</td>
+                <td>AutoCAD II</td>
+                <td>4</td>
+                <td>
+                    <input type="checkbox" name="courseSelected[]" />
+                </td>
+            </tr>
+            <tr>
+                <td>2017</td>
+                <td>Fall</td>
+                <td>CON8418</td>
+                <td>Construction Building Code</td>
+                <td>3</td>
+                <td>
+                    <input type="checkbox" name="courseSelected[]" />
+                </td>
+            </tr>
+            <tr>
+                <td>2017</td>
+                <td>Fall</td>
+                <td>ENG8454</td>
+                <td>Geotechnical Materials</td>
+                <td>3</td>
+                <td><input type="checkbox" name="courseSelected[]" /></td>
+            </tr>
+            <tr>
+                <td>2017</td>
+                <td>Fall</td>
+                <td>MGT8400</td>
+                <td>Project Adminstration</td>
+                <td>4</td>
+                <td><input type="checkbox" name="courseSelected[]" /></td>
+            </tr>
+            <tr>
+                <td colspan="3"></td>
+                <td>Total Weekly Hours</td>
+                <td colspan="2">14</td>
+            </tr>
+            <tr>
+                <td>2017</td>
+                <td>Winter</td>
+                <td>CON8102</td>
+                <td>Commercial Building/Estimating</td>
+                <td>5</td>
+                <td><input type="checkbox" name="courseSelected[]" /></td>
+            </tr>
+            <tr>
+                <td colspan="3"></td>
+                <td>Total Weekly Hours</td>
+                <td colspan="2">5</td>
+            </tr>
+            <tr>
+                <td>2018</td>
+                <td>Fall</td>
+                <td>CST8110</td>
+                <td>Introduction to Computer Programming</td>
+                <td>4</td>
+                <td><input type="checkbox" name="courseSelected[]" /></td>
+            </tr>
+            <tr>
+                <td>2018</td>
+                <td>Fall</td>
+                <td>CST8209</td>
+                <td>Web Programming I</td>
+                <td>4</td>
+                <td><input type="checkbox" name="courseSelected[]" /></td>
+            </tr>
+            <tr>
+                <td>2018</td>
+                <td>Fall</td>
+                <td>CST8260</td>
+                <td>Database System and Concepts</td>
+                <td>3</td>
+                <td><input type="checkbox" name="courseSelected[]" /></td>
+            </tr>
+        </table>
+        <input type="button" name="deleteSelected" value="Delete Selected" />
+        <input type="reset" name="clear" value="Clear" />
+    </form>
 </div>
 
 <?php include "Common/Footer.php"; ?>
