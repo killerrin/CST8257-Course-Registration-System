3790348a49caf1077f336448cbfd751a75bc3aa8
Andrew Godfroy <andrew.godfroy@killerrin.com>
2017-12-20 23:21:36 -0500

    Repo Gatherings

--- Lab 7/CurrentRegistration.php
+++ Lab 7/CurrentRegistration.php
@@ -1,6 +1,16 @@
 <?php //session_start(); ?>
 <?php include "Common/Header.php";
 $LoggedInUser = isset($_SESSION["LoggedInUser"]) ? $_SESSION["LoggedInUser"] : (function() { header("Location: Login.php?returnUrl=".urlencode($_SERVER['REQUEST_URI'])); die();})();
+
+$dbManager = new DBManager();
+$courseRepo = new DBCourseRepository($dbManager);
+$semesterRepo = new DBSemesterRepository($dbManager);
+$registration = new DBRegistrationRepository($dbManager);
+$dbManager->connect();
+$allCourses = $courseRepo->getAll();
+$allSemesters = $semesterRepo->getAll();
+$allUserRegistrations = $registration->getForUser($LoggedInUser->studentID);
+$dbManager->close();
 ?>
 
 <div class="container">
